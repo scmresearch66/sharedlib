7006404f9549e8962a4dc5f668db3a369bee788b
Ralph Boerger <ralph.boerger@gmail.com>
2017-02-22 18:42:12 +0000

    initial

--- src/org/scmresearch66/Zot.groovy
+++ src/org/scmresearch66/Zot.groovy
@@ -1,11 +1,18 @@
 package org.scmresearch66;
 
-def checkOutFrom(repo) {
-   echo "Here"
-
-   def causes = currentBuild.rawBuild.getCauses()
-   def specificCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
-
-   echo "${causes}"
-   echo "${specificCause}"
+@NonCPS
+def showChangeLogs() {
+  def changeLogSets = currentBuild.rawBuild.changeSets
+  for (int i = 0; i < changeLogSets.size(); i++) {
+     def entries = changeLogSets[i].items
+     for (int j = 0; j < entries.length; j++) {
+          def entry = entries[j]
+          echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+          def files = new ArrayList(entry.affectedFiles)
+          for (int k = 0; k < files.size(); k++) {
+              def file = files[k]
+              echo "  ${file.editType.name} ${file.path}"
+          }
+      }
+  }
 }
