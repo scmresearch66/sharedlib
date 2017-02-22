0f5648e03af0156c0b11c9d3f8bb2ea81363c59c
Ralph Boerger <ralph.boerger@gmail.com>
2017-02-22 18:37:06 +0000

    initial

--- src/org/scmresearch66/Zot.groovy
+++ src/org/scmresearch66/Zot.groovy
@@ -4,6 +4,8 @@ def checkOutFrom(repo) {
    echo "Here"
 
    def causes = currentBuild.rawBuild.getCauses()
+   def specificCause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
 
    echo "${causes}"
+   echo "${specificCause}"
 }
