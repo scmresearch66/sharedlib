227173842f64b23490d1ebe9ff439e799003eae1
Ralph Boerger <ralph.boerger@gmail.com>
2017-02-22 17:15:12 +0000

    initial

--- src/org/scmresearch66/Point.groovy
+++ src/org/scmresearch66/Point.groovy
@@ -1,6 +1,6 @@
 package org.scmresearch66;
 
-def removeTrailingSlash (String myString){
+def removeTrailingSlash(String myString) {
   if (myString.endsWith("/")) {
     return myString.substring(0, myString.length() - 1);
   }
